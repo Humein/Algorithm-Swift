bce0982c8fbc1a7a4a81714e6c91eb892b8eeb11
zhangxinxin <zhangxinxin12@100tal.com>
2020-10-15 21:12:41 +0800

    2

--- 方法分类/递归/对称的二叉树 - Symmetric.playground/Contents.swift
+++ 方法分类/递归/对称的二叉树 - Symmetric.playground/Contents.swift
@@ -11,6 +11,7 @@ class TreeNode {
         self.right = right
     }
 }
+
 //offer28：对称的二叉树
 /*
  请实现一个函数，用来判断一棵二叉树是不是对称的。如果一棵二叉树和它的镜像一样，那么它是对称的。
@@ -29,9 +30,7 @@ class TreeNode {
     \   \
     3    3
 
- 来源：力扣（LeetCode）
- 链接：https://leetcode-cn.com/problems/dui-cheng-de-er-cha-shu-lcof
- 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
  递归
  1. 定义递归函数
  2. 边界问题 递归结束条件
@@ -60,4 +59,21 @@ func isSymmetrics(_ node1: TreeNode?,_ node2: TreeNode?)-> Bool {
     // 3   入站 这些逻辑都是在入站操作中处理了
     return isSymmetrics(node1?.left, node2?.right) && isSymmetrics(node1?.right, node2?.left)
     
+}
+
+// 2020.10.12
+/// 引入两个入参来分解 左右树
+func symmetryTrees(_ node1: TreeNode?,_ node2: TreeNode?) -> Bool {
+    if node1 == nil || node2 == nil{
+        return false
+    }
+    if node1 == nil && node2 == nil {
+        return true
+    }
+    /// 不能判定相等的条件；只需要判断不等直到tree遍历完成就行
+    if node1?.val != node2?.val {
+        return false
+    }
+    // 遍历并判断左右tree
+    return symmetryTrees(node1?.left, node2?.right) && symmetryTrees(node1?.right, node2?.left)
 }
